5424c5fe9000401c363e03bd108f8ddcbfa12b29
GencoMattia <gencomattia@hotmail.it>
2024-05-20 13:22:54 +0200

    js: created UniqueRandomNumber function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
+const gridEl = document.querySelector("#grid");
 const getDifficulty = 100;
 
-function getNewGame(difficulty) {
+function getNewGame(container, difficulty) {
     document.querySelector("#play-button").addEventListener("click", function(){
-    const gridEl = document.querySelector("#grid");
         gridEl.innerHTML = ""
         for(i = 0; i < difficulty; i++) {
             // Elements
@@ -24,9 +24,21 @@ function getNewGame(difficulty) {
 };
 
 function getRandomNumber(min, max) {
-return Math.floor( Math.random() * (max - min + 1)) + min;
+    return Math.floor( Math.random() * (max - min + 1)) + min;
+};
+
+function getUniqueRandomNumber(blacklist, min, max) {
+    let randomNumber;
+    let isFound = false;
+    while(!isFound) {
+        randomNumber = getRandomNumber(min, max);
+        if(blacklist.includes(randomNumber) !== false){
+            isFound = true; 
+        };
+    };
+    return randomNumber;
 };
 
 document.querySelector("#play-button").addEventListener("click", function(){
-    getNewGame(getDifficulty);
+    getNewGame(gridEl, getDifficulty);
 });
