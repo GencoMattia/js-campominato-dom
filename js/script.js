5390fb3c8162c77a3329bc71e45879ff582c996f
GencoMattia <gencomattia@hotmail.it>
2024-05-20 12:45:11 +0200

    created getRandomNumber function

--- js/script.js
+++ js/script.js
@@ -19,10 +19,14 @@ function getNewGame(difficulty) {
                 cellEl.classList.add("active");
                 console.log(cellNumberEl.outerText);
             });
-        }
+        };
     });
-}
+};
+
+function getRandomNumber(min, max) {
+return Math.floor( Math.random() * (max - min + 1)) + min;
+};
 
 document.querySelector("#play-button").addEventListener("click", function(){
-    getNewGame(getDifficulty)
-})
+    getNewGame(getDifficulty);
+});
